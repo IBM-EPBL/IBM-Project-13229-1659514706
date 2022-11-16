8c5f0583efbceb8cbfbc3c2d2eb6a1db5d89e2b9
Kousik Nibith Ram V P <66196561+Kousiknibith13@users.noreply.github.com>
2022-11-16 18:18:27 +0530

    Flask and Webpage update

--- Project_Development_Phase/Sprint2_and_Sprint3/app.py
+++ Project_Development_Phase/Sprint2_and_Sprint3/app.py
@@ -2,18 +2,19 @@ import numpy as np
 from flask import Flask,jsonify,request,render_template
 import pickle
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 model=pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
-def predict():
+def Home():
     return render_template('Manual_predict.html')
 
-@app.route('/y_predict', methods=['POST'])
-def y_predict():
-    x_test=[[float(x) for x in request.form.values()]]
-    print('actual',x_test)
-    pred=model.predict(x_test)
-    return render_template("Manual_predict.html", prediction_text = ("Car Fuel Consumption(L/100km) : {}",pred[0]))
+@app.route('/predict', methods=['POST'])
+def predict():
+    float_features=[float(x) for x in request.form.values()]
+    features=[np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("Manual_predict.html", prediction_text = "Car Fuel Consumption(L/100km) : {}".format(prediction))
+
 if __name__ == "__main__":
     app.run(debug=True)
